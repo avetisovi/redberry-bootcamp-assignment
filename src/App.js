994eaf60d68fdd57fede2a10a8eb6c48bfb74c46
Anatoli Avetisovi <tolikatolika794@gmail.com>
2022-08-27 12:02:24 +0400

    initial files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './fonts/HelveticaNeue.ttc';
+import './styles/index.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
