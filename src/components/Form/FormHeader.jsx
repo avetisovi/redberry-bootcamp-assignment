075a1916d616723822b14e1243ca117d039efcca
Anatoli Avetisovi <tolikatolika794@gmail.com>
2022-08-31 15:39:41 +0400

    added responsive design for form

--- src/components/Form/FormHeader.jsx
+++ src/components/Form/FormHeader.jsx
@@ -8,9 +8,10 @@ const FormHeader = ({ step }) => {
           step === 1 ? 'form__header-steps one' : 'form__header-steps two'
         }
       >
-        <p className="form__header-step">თანამშრომლის ინფო</p>
-        <p className="form__header-step">ლეპტოპის მახასიათებლები</p>
+        <p className="form__header-step first">თანამშრომლის ინფო</p>
+        <p className="form__header-step second">ლეპტოპის მახასიათებლები</p>
       </div>
+      <p className="step__counter">{step}/2</p>
     </div>
   );
 };
